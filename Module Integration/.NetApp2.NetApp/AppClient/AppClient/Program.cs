c9df9b0dbe66a08e0126186323cb55ed70ee7151
Brasse2873 <github@titkb.se>
2016-09-15 09:34:55 +0200

    Added Module Integration Test apps

---
+++ Module Integration/.NetApp2.NetApp/AppClient/AppClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AppServer;
+
+namespace AppClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassInAppServer o = new ClassInAppServer();
+            o.IntPar = 100;
+            Console.WriteLine("ClassInAppServer.IntPar="+ o.IntPar);
+        }
+    }
+}
---
+++ Module Integration/.NetApp2.NetLib/NetApp2NetLib/AppClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AppClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LibServer.ClassInLibServer o = new LibServer.ClassInLibServer();
+            o.IntPar = 100;
+            Console.WriteLine("ClassInLibServer.IntPar="+o.IntPar);
+        }
+    }
+}
